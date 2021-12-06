d0af1d69d695dd9d727837d6e3de3ce098f18651
Mary Ellen <85257788+marynhaSjc@users.noreply.github.com>
2021-12-06 19:36:38 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -25,33 +24,5 @@ def contact():
     return render_template("contato.html")
 
 
-=======
-from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route("/") 
-@app.route("/index") 
-def home(): 
-    return render_template("index.html")
-
-@app.route("/perfil")
-def profile():
-    return render_template("perfil.html")
-
-@app.route("/experiencia")
-def experience():
-    return render_template("experiencia.html")
-
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
-
-@app.route("/contato")
-def contact():
-    return render_template("contato.html")
-
-if __name__== "__main__":
-    app.run(debug=True) #host='0.0.0.0'
->>>>>>> 4002d99cb4b5be2f4fcd93f3273066533a811158
    
